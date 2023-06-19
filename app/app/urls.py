a12745289167149ad899da8bdf7f2078bcfaf873
peterrmah <peter.r.mah@gmail.com>
2023-06-19 11:53:05 -0600

    feat: init recipe app

--- app/app/urls.py
+++ app/app/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
         name='api-docs'
     ), # schema displayed in swagger
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Tests for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTest(TestCase):
@@ -38,7 +42,7 @@ class ModelTest(TestCase):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
-        """test creating a superuser."""
+        """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
@@ -46,3 +50,19 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description.'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
